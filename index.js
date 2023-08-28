6f3adcdd84986559f034c241863492a3471ee47b
Justine <Justinegeechu333@gmail.com>
2023-08-27 20:35:10 -0500

    complete loop

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+	const arr = [];
+	for (let i = 0; i < names.length; i++) {
+		arr.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`);
+	}
+	return arr;
+}
+
+function countDown(startNum) {
+	for (let i = startNum; i >= 0; i--) {
+		console.log(i);
+	}
+}
